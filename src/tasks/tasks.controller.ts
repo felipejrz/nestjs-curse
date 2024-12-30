5d257a57baa4fdeaf8207e0d3d9378efd7880339
Felipe Daniel Juarez Alvarez <spider_spainfel@hotmail.com>
2024-12-30 11:48:25 -0600

    add service task and module tasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Delete, Get, Post, Put, Body } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Post,
+  Put,
+  Body,
+  Param,
+} from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -8,18 +17,21 @@ export class TasksController {
   }
 
   @Post()
-  createTasks(@Body() task): string {
+  createTasks(@Body() task: CreateTaskDto): string {
     console.log(task);
     return 'tasks';
   }
 
-  @Put()
-  updateTasks(): string {
-    return 'tasks';
+  @Put(':id')
+  updateTasks(@Body() task: CreateTaskDto, @Param('id') id): string {
+    console.log(task);
+    console.log(id);
+    return 'Updating a tasks';
   }
 
-  @Delete()
-  deleteTask(): string {
-    return 'tasks';
+  @Delete(':id')
+  deleteTask(@Param('id') id): string {
+    console.log(id);
+    return `Deleting a task number: ${id}`;
   }
 }
